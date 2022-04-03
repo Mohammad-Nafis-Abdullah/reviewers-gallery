db0a24ca9f8298383475bd02dd6a7f5ebb574ccb
Muhammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2022-04-04 02:34:40 +0600

    header component nav bar created with responsiveness

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import Open from '../bars-solid.svg'
+import Close from '../xmark-solid.svg'
 
 const Header = () => {
+    const [menu,setMenu] = useState(['h-0','h-auto',Open])
+
+    const closeMenu = ()=> {
+        menu[2]===Open?setMenu(['h-60','h-screen',Close]):setMenu(['h-0','h-auto',Open]);
+    }
+
     return (
-        <div className='shadow-md'>
-            <section className='container mx-auto'>
-                <nav className='flex justify-center items-center gap-10 h-20 border-2 border-black'>
+        <div className={`${menu[1]} shadow-md fixed top-0 left-0 right-0 z-50 bg-black/10 sm:bg-white`}>
+            <section className={`duration-500 container mx-auto flex justify-between p-5 sm:justify-center bg-white`}>
+                <img className='sm:hidden w-10 h-12 active:scale-90 transition cursor-pointer' src={menu[2]} alt="" onClick={closeMenu} />
+                <nav className={`${menu[0]} transition-[height] overflow-hidden duration-300 grow px-2 flex flex-col gap-5 justify-start items-end sm:flex-row sm:justify-center sm:items-center sm:px-0 sm:grow-0 sm:h-10 sm:overflow-visible sm:transition-none`}>
                     <NavLink to='/home' className={(navInfo)=> navInfo.isActive?'nav-link-active':'nav-link'}>Home</NavLink>
                     <NavLink to='/reviews' className={(navInfo)=> navInfo.isActive?'nav-link-active':'nav-link'}>Reviews</NavLink>
                     <NavLink to='/dashboard' className={(navInfo)=> navInfo.isActive?'nav-link-active':'nav-link'}>Dashboard</NavLink>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div className=''>
-                        
+        <div className='container mx-auto border-4 border-black mt-32'>
+            <div>
+                <h1>Our Watches are just for your happines</h1>
+                <p></p>
+            </div>
         </div>
     );
 };
