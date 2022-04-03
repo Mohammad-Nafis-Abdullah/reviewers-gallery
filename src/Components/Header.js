26e9282747bf457402dee481aea52f2dc0ca659c
Muhammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2022-04-04 00:56:17 +0600

    404 not found page added with complete design

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,9 +3,9 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
+        <div className='shadow-md'>
             <section className='container mx-auto'>
-                <nav className='flex justify-center items-center gap-8 h-20 shadow-md '>
+                <nav className='flex justify-center items-center gap-10 h-20 border-2 border-black'>
                     <NavLink to='/home' className={(navInfo)=> navInfo.isActive?'nav-link-active':'nav-link'}>Home</NavLink>
                     <NavLink to='/reviews' className={(navInfo)=> navInfo.isActive?'nav-link-active':'nav-link'}>Reviews</NavLink>
                     <NavLink to='/dashboard' className={(navInfo)=> navInfo.isActive?'nav-link-active':'nav-link'}>Dashboard</NavLink>
--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const NotFound = () => {
 
     return (
-        <div className='container h-screen mx-auto flex flex-col gap-20 justify-center items-center'>
-            <Link to='/home' className='btn-return'>Return Home</Link>
-            <div className='px-3'>
-                <h1 className='text-center text-white drop-shadow-[0px_5px_5px_#ff0000c6] text-8xl sm:text-[15rem] font-bold cursor-not-allowed animate-bounce'>
-                    404
-                </h1>
-                <p className='text-center text-xl sm:text-6xl drop-shadow-[0px_0px_0] text-red-600/80 font-bold leading-none'>
+        <div className='container h-[calc(100vh-80px)] mx-auto flex justify-center items-center'>
+            <div className=''>
+                <h1 className='text-center text-white drop-shadow-[0px_5px_5px_#ff0000c6] text-8xl sm:text-[15rem] font-bold cursor-not-allowed animate-bounce leading-[0.7]'>404</h1>
+                <p className='text-center text-xl sm:text-6xl drop-shadow-[0px_0px_0] text-red-600/80 font-bold'>
                     Page Not Found
                 </p>
             </div>
