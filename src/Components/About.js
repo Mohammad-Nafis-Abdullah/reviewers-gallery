d28caf1e378293524c858f7f5a30c5e05c0ebdad
Muhammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2022-04-05 15:25:30 +0600

    animation added in the about route cards

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,12 +3,12 @@ import React from 'react';
 const About = () => {
     return (
         <div className='flex justify-center items-center mt-32 gap-3'>
-            <div className='basis-80 border shadow-md bg-slate-400/10 rounded p-3 '>
+            <div className='max-w-xs border shadow-md bg-slate-400/10 rounded p-3 fromLeft'>
                 <img src="https://i.pinimg.com/originals/f1/13/90/f113905fe574931e323146cbf829edc1.jpg" alt="" className='max-h-40 w-full object-cover object-top rounded'/>
                 <h3 className='text-xl text-gray-900 font-medium my-3 uppercase'>Chittagong Office :</h3>
                 <p className='text-lg font-medium text-gray-800 underline decoration-2'>Address :</p><address className='text-lg font-medium text-gray-700 no-underline'>Lane No. 02, Road No. 01, K-Block R/A, Halishahar, Chittagong</address>
             </div>
-            <div className='basis-80 border shadow-md bg-slate-400/10 rounded p-3'>
+            <div className='max-w-xs border shadow-md bg-slate-400/10 rounded p-3 fromRight'>
                 <img src="https://thumbs.dreamstime.com/b/bangladesh-april-top-views-empty-shapla-chattar-motijheel-coronavirus-shutdown-movement-dhaka-178442642.jpg" alt="" className='max-h-40 w-full object-cover rounded' />
                 <h3 className='text-xl text-gray-900 font-medium my-3 uppercase'>Dhaka Office :</h3>
                 <p className='text-lg font-medium text-gray-800 underline decoration-2'>Address :</p><address className='text-lg font-medium text-gray-700 no-underline'>14/a, Road No. 05, Elephant Road, Dhaka</address>
